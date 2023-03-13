b31bcaf3b2108677388350c3376da165b4368834
mturkcelik <88403649+mturkcelik@users.noreply.github.com>
2023-03-14 01:18:40 +0300

    nothing

--- ppm/subscriptions/models.py
+++ ppm/subscriptions/models.py
@@ -4,6 +4,7 @@ from djstripe.models import Customer, Subscription
 
 
 class CustomUser(AbstractUser):
-    subscription = models.ForeignKey(Subscription, null=True, blank=True,on_delete=models.SET_NULL)
+    subscription = models.ForeignKey(Subscription, null=True, blank=True, on_delete=models.SET_NULL)
     customer = models.ForeignKey(Customer, null=True, blank=True, on_delete=models.SET_NULL)
+
     #pass
